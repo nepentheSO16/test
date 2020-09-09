446dbd31b3bf6c42ac9662875b28433fae56bbdd
nepentheSO16 <1357310062@qq.com>
2020-09-09 18:34:13 +0800

    9-9

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 #include<string> 
 #include<cmath>
+#define pi 3.14159265358979
 using namespace std;
-int jinzhizhuanhuan(string a,int h,int n){
-	int l=0;
+double jinzhizhuanhuan(string a,double h,double n){
+	int j=n;
 	for(int i=0;i<n;i++){
-		l=l+(int)(a[n-i-1]-'0')*pow(h,i);	
+		if(a[i]=='.') j=i+1;
+	}
+	double l=0;
+	for(int i=j-n,I=0;I<n;I++,i++){
+		if(a[n-I-1]=='.') {
+			I++;
+		}
+		l=l+(double)(a[n-I-1]-'0')*pow(h,i);	
 	}
 	return l;
 }
-int jinzhishibie(string a){
-	int i;
+double jinzhishibie(string a){
+	double i;
 	int n=a.length();
 	if(a[0]=='0'){
 		if(a[1]=='b'||a[1]=='B'){
@@ -30,7 +38,7 @@ int jinzhishibie(string a){
 	}
 	return jinzhizhuanhuan(a,i,n);
 }
-int sizeyunsuan(){
+double sizeyunsuan(){
 	string a;
 	cin>>a;
 	int l=1,L=1;
@@ -39,9 +47,9 @@ int sizeyunsuan(){
 		if(a[i]=='-'||a[i]=='+') L++; 
 	}
 	string b;
-	int c[l],C[L];//记录一共有多少个数以及有多少数进行加减运算 
+	double c[l],C[L];//记录一共有多少个数以及有多少数进行加减运算 
 	char d[l-1],D[L-1];//记录一共有多少运算符以及有多少加减运算符 
-	int I=0;
+	double I=0;
 	int j=0,J=0;
 	for(int i=0;i<=a.length();i++,I++){
 		if(a[i]=='+'||a[i]=='-'||a[i]=='*'||a[i]=='/'){
@@ -80,13 +88,115 @@ int sizeyunsuan(){
 	}
 	return I;
 }
+double duishuyunsuan(){//对数形式只支持logx（Y）（x为底数） 
+	string a;
+	cin>>a;
+	string b,c;
+	int j=0,J=0;
+	for(int i=0;i<a.length();i++){
+		if(a[i]=='(') j=i;
+		if(a[i]==')'){
+			J=i;
+			break;
+		}
+	}
+	b=a.substr(3,j-3);
+	c=a.substr(j+1,J-j-1);
+	double A;
+	double B,C;
+	B=jinzhishibie(b);
+	C=jinzhishibie(c);
+	A=log(double(C))/log(double(B));
+	return A;
+}
+double miyunsuan(){
+	string a;
+	cin>>a;
+	int j;
+	for(int i=0;i<a.length();i++){
+		if(a[i]=='*'){
+			j=i;
+			break; 
+		}
+	}
+	string b,c;
+	b=a.substr(0,j);
+	c=a.substr(j+2,a.length()-j-2);
+	double A,B,C;
+	B=jinzhishibie(b);
+	C=jinzhishibie(c);
+	cout<<B<<" "<<C;
+	A=pow(B,C);
+	return A;
+} 
+double sanjiaohanshu(){
+	string a;
+	cin>>a;
+	string b,c;
+	b=a.substr(0,3);
+	c=a.substr(4,a.length()-5);
+	double A,C;
+	C=jinzhishibie(c);
+	C=C/180.0*pi;
+	if(b=="cos") A=cos(C);
+	else if(b=="sin") A=sin(C);
+	else if(b=="tan") A=tan(C);
+	else if(b=="cot") A=1.0/tan(C);
+	else if(b=="sec") A=1.0/cos(C);
+	else if(b=="csc") A=1.0/sin(C);
+	return A;
+}
+void bijiao(){
+	string a,b;
+	cin>>a;
+	cin>>b;
+	double A,B;
+	A=jinzhishibie(a);
+	B=jinzhishibie(b);
+	cout<<"比较的结果为：";
+	if(A>B) cout<<a<<">"<<b<<endl;
+	else if(A==B) cout<<a<<"="<<b<<endl;
+	else if(A<B) cout<<a<<"<"<<b<<endl; 
+} 
+void gongyueshugongbeishu(){
+	string a,b;
+	cin>>a;
+	cin>>b;
+	double A,B;
+	A=jinzhishibie(a);
+	B=jinzhishibie(b);
+	cout<<"公约数有："; 
+	for(int i=1;i<=min(A,B);i++){
+		if((int)(A)%i==0&&(int)(B)%i==0){
+			cout<<i<<" ";
+		}
+	}
+	cout<<endl;
+	for(int i=max(A,B);1;i++){
+		if(i%(int)(A)==0&&i%(int)(B)==0){
+			cout<<"最小公倍数为："<<i<<endl;
+			break; 
+		}
+	}
+}
 int main(){
 	cout<<"请选择您想要完成的功能："<<endl;
-	cout<<"四则运算请输入1";
+	cout<<"四则运算请输入1   十进制对数运算请输入2   幂运算请输入3   三角函数计算请输入4   任意两个进制数字之间比较运算请输入5"<<endl;
+	cout<<"公约数、最小公倍数计算请输入6"<<endl;
 	int i;
 	cin>>i;
 	switch(i){
-		case 1: cout<<"输入式的结果转换为十进制为："<<sizeyunsuan();
+		case 1: cout<<"输入式的结果为："<<sizeyunsuan();
+				break;
+		case 2: cout<<"输入式对数运算的结果为："<<duishuyunsuan();
+				break;
+		case 3: cout<<"幂运算的结果为："<<miyunsuan();
+				break;
+		case 4: cout<<"三角函数的计算结果为："<<sanjiaohanshu();
+				break;
+		case 5: bijiao();
+				break;
+		case 6: gongyueshugongbeishu();
 				break;
 	}	
 } 
